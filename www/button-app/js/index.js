d4f7445a634d0d9cafd447d7bef86545f7b5b40b
Eric Butler <ericbutler000@gmail.com>
2015-08-22 19:03:40 -0400

    Made media platform agnostic, I think.

--- www/button-app/js/index.js
+++ www/button-app/js/index.js
@@ -1,13 +1,24 @@
 var Main = {
-	wavs: ['moo1', 'moo2', 'chicken1', 'duck1', 'pig1', 'sheep1'],
-	init: function() {
-		$('.the-button' ).on('click', function() {
+	wavNames: ['moo1', 'moo2', 'chicken1', 'duck1', 'pig1', 'sheep1'],
+	wavs    : [],
+	init    : function() {
+		var isAndroid = device.platform.toLowerCase() == 'android';
+		Main.wavNames.map( function( ele ) {
+			var url = 'sounds/' + ele + '.wav';
+			url = isAndroid ? '/android_asset/www/' + url : url;
+			Main.wavs.push( new Media( url ) );
+		} );
+
+		$( '.the-button' ).on( 'click', function() {
 			// PLAY SOUND
-			var wavNum = Math.floor(Math.random() * Main.wavs.length);
-			$('#' + Main.wavs[wavNum] )[0].play();
-		});
+			var wavNum = Math.floor( Math.random() * Main.wavs.length );
+			Main.wavs[wavNum].play()
+		} );
 	}
 };
 
-Main.init();
+function onDeviceReady() {
+	Main.init();
+}
 
+document.addEventListener( 'deviceready', onDeviceReady, false );
